6f7c273fea1e4f64587242ef0cf71f1d32cdfd98
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2024-05-21 23:19:49 -0600

    Minor tests

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front';
+  city = 'Barcelona';
 }
