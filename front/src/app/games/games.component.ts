b6ed295b678b10cc963d87c714a5f17a4d609c16
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2024-05-24 14:20:40 -0600

    minor changes front

---
+++ front/src/app/games/games.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [],
+  template: `
+    <ul>
+      @for (game of games; track game.id) {
+        <li>{{game.name}}</li>
+      }
+    </ul>
+
+  `,
+  styles: ``
+})
+export class GamesComponent {
+
+  @Input() username = '';
+
+  games = [
+    {
+    id: 1,
+    name: 'Uncharted 4'
+  },
+  {
+    id: 2,
+    name: 'Horizon Zero Dawn'
+  },
+  {
+    id: 3,
+    name: 'Bloodborne'
+  }
+]
+
+}
